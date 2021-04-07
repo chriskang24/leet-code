b1c90fbf16d3506d0de02e7a636217a5fbccf97d
chriskang24 <christopherjkang24@gmail.com>
2021-04-07 14:19:22 -0400

    complete find sum of two values in array exec

---
+++ twoSum.js
@@ -0,0 +1,30 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+const twoSum = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+};
+
+let nums = [22, 11, 5, 17]
+let target = 39
+twoSum(nums, target)
+
+// const twoSum = function (nums, target) {
+
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
